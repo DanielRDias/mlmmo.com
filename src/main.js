227e3608c106def09fdec15924829682221bef35
Daniel Dias <danielricardodias@gmail.com>
2021-01-20 01:27:41 +0000

    feat(auth): added auth store and login component

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "@aws-amplify/ui-vue";
-import Amplify from "aws-amplify";
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
 Vue.config.productionTip = false;
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,41 +1,17 @@
 <template>
-  <div id="AuthWithSlots">
-    <amplify-authenticator>
-      <amplify-sign-up
-        slot="sign-up"
-        :form-fields.prop="formFields"
-      ></amplify-sign-up>
-      <amplify-sign-in slot="sign-in"></amplify-sign-in>
-    </amplify-authenticator>
-    <!-- <vue-recaptcha
-      v-if="customConfirmSignIn"
-      sitekey="6LcjwjEaAAAAAOr6mjLL-8I0yMQuOfZ1G2E0Pl-T"
-      @verify="onVerify"
-    ></vue-recaptcha> -->
+  <div class="home">
+    <Login />
   </div>
 </template>
 
 <script>
-// import VueRecaptcha from "vue-recaptcha";
+// @ is an alias to /src
+import Login from "@/components/Login.vue";
+
 export default {
-  name: "AuthWithSlots",
-  data() {
-    return {
-      formFields: [
-        {
-          type: "username",
-          required: true,
-        },
-        {
-          type: "password",
-          required: true,
-        },
-        {
-          type: "email",
-          required: true,
-        },
-      ],
-    };
+  name: "Home",
+  components: {
+    Login,
   },
 };
 </script>
