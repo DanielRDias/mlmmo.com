ee2e3d47cd26f311602491d459f2026a2e94c428
Daniel Dias <danielricardodias@gmail.com>
2021-02-07 22:20:57 +0000

    refactor(app): renamed app to mlarpg

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title"> MLMMO.com</v-list-item-title>
-          <v-list-item-subtitle>Magic Legends MMO</v-list-item-subtitle>
+          <v-list-item-title class="title"> MLaRPG.com</v-list-item-title>
+          <v-list-item-subtitle>Magic Legends ARPG</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
