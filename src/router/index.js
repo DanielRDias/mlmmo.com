98fa3bae344bd5f5018077c6a950645f0c2f113b
Daniel Dias <danielricardodias@gmail.com>
2021-02-15 01:14:43 +0000

    feat(router): added page title

--- src/router/index.js
+++ src/router/index.js
@@ -11,31 +11,56 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      requiresAuth: false,
+      title: "Magic: Legends ARPG - Best deck, class, cards and artifacts",
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      requiresAuth: false,
+      title: "Login | Magic: Legends ARPG",
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: () => import("../views/Register.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Register - Create new account | Magic: Legends ARPG",
+    },
   },
   {
     path: "/classes",
     name: "Classes",
     component: () => import("../views/Classes.vue"),
+    meta: {
+      requiresAuth: false,
+      title:
+        "Classes - Sanctifier, Mindmage, Necromancer, Geomancer && Beastcaller | Magic: Legends ARPG",
+    },
   },
   {
     path: "/cards",
     name: "Cards",
     component: () => import("../views/Cards.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Cards - White, Blue, Black, Red && Green | Magic: Legends ARPG",
+    },
   },
   {
     path: "/card",
     name: "Card",
     component: () => import("../views/Card.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Magic: Legends Card | Magic: Legends ARPG",
+    },
   },
   {
     path: "/addcards",
@@ -47,21 +72,37 @@ const routes = [
     path: "/decks",
     name: "Decks",
     component: () => import("../views/Decks.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Decks - Best Magic Legends Decks | Magic: Legends ARPG",
+    },
   },
   {
     path: "/deck",
     name: "Deck",
     component: () => import("../views/Deck.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Magic: Legends Deck | Magic: Legends ARPG",
+    },
   },
   {
     path: "/adddeck",
     name: "AddDeck",
     component: () => import("../views/AddDeck.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Create Deck - Create your own Deck | Magic: Legends ARPG",
+    },
   },
   {
     path: "/artifacts",
     name: "Artifacts",
     component: () => import("../views/Artifacts.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Artifacts - Lesser, Greater && Legendary | Magic: Legends ARPG",
+    },
   },
   {
     path: "/contentcreators",
@@ -87,6 +128,15 @@ const routes = [
     name: "TermsConditions",
     component: () => import("../views/TermsConditions.vue"),
   },
+  {
+    path: "/beta",
+    name: "Beta",
+    component: () => import("../views/Beta.vue"),
+    meta: {
+      requiresAuth: false,
+      title: "Magic: Legends Open Beta release date",
+    },
+  },
 ];
 
 const router = new VueRouter({
@@ -97,13 +147,60 @@ const router = new VueRouter({
 
 router.beforeEach(async (to, from, next) => {
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
-  const isAuthenticated = await Auth.currentUserInfo();
 
-  if (requiresAuth && !isAuthenticated) {
-    next("/");
-  } else {
-    next();
+  if (requiresAuth) {
+    const isAuthenticated = await Auth.currentUserInfo();
+    if (!isAuthenticated) {
+      next("/");
+    }
   }
+  const defaultTitle =
+    "Magic: Legends ARPG - Best deck, class, cards and artifacts";
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
+  // `/nested`'s will be chosen.
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  } else {
+    document.title = defaultTitle;
+  }
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(
+    document.querySelectorAll("[data-vue-router-controlled]")
+  ).map((el) => el.parentNode.removeChild(el));
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next();
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags
+    .map((tagDef) => {
+      const tag = document.createElement("meta");
+
+      Object.keys(tagDef).forEach((key) => {
+        tag.setAttribute(key, tagDef[key]);
+      });
+
+      // We use this to track which meta tags we create so we don't interfere with other ones.
+      tag.setAttribute("data-vue-router-controlled", "");
+
+      return tag;
+    })
+    // Add the meta tags to the document head.
+    .forEach((tag) => document.head.appendChild(tag));
 });
 
 export default router;
