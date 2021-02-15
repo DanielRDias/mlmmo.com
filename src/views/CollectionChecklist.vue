1ba673a05258a8583a9ddb70395d1180876d85b4
Daniel Dias <danielricardodias@gmail.com>
2021-02-15 22:26:38 +0000

    feat(collection checklist): added collection checklist

---
+++ src/views/CollectionChecklist.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card light width="100%" height="100%">
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/1unptW-TaBJKwXWFOtTW8Hfh-d7PCIlJ06oEAx9L_b_s"
+      width="100%"
+      height="100%"
+    ></iframe>
+  </v-card>
+</template>
