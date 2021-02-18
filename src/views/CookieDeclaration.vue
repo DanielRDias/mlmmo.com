6e5bbeecad8a9f1b731804c3718006bab029c707
Daniel Dias <danielricardodias@gmail.com>
2021-02-18 18:41:22 +0000

    chore(cookie): update cookie concent declaration

--- src/views/CookieDeclaration.vue
+++ src/views/CookieDeclaration.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="cookiedeclaration">
-    <CookieDeclaration />
-  </div>
+  <v-card light width="100%" height="100%">
+    <iframe src="cookie-concent.html" width="100%" height="100%"></iframe>
+  </v-card>
 </template>
-
-<script>
-import CookieDeclaration from "@/components/CookieDeclaration.vue";
-
-export default {
-  components: {
-    CookieDeclaration,
-  },
-};
-</script>
