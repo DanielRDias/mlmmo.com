1b058d1f321406fcb58776ecfaf3031d6e1bce23
Daniel Dias <danielricardodias@gmail.com>
2021-02-18 20:48:52 +0000

    chore(cookie): update manage cookies link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -124,7 +124,11 @@
       </v-timeline-item>
     </v-timeline>
     <div id="CookieDeclarationUserStatusPanel">
-      <a href="/cookiedeclaration">Cookie Declaration User Status Panel</a>
+      <p align="right">
+        <v-btn text href="/cookiedeclaration" x-small>
+          Manage Cookie Preferences
+        </v-btn>
+      </p>
     </div>
   </div>
 </template>
