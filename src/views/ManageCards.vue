e2b6590fda145a924077e2c1ba6251273924566f
Daniel Dias <danielricardodias@gmail.com>
2021-03-05 04:06:09 +0000

    feat(manage cards): cards updates, submissions and versioning

---
+++ src/views/ManageCards.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <ManageCards :currentCardId="currentCardtId" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ManageCards from "@/components/ManageCards.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ManageCards,
+  },
+  data() {
+    return {
+      currentCardtId: this.$route.query.cardId,
+    };
+  },
+};
+</script>
