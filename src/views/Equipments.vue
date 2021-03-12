32afa029f8b44274d0033b94254d95396a698e4f
Daniel Dias <danielricardodias@gmail.com>
2021-03-12 14:45:24 +0000

    feat(equipment): added equipment system

---
+++ src/views/Equipments.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Equipments />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Equipments from "@/components/Equipments.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Equipments,
+  },
+};
+</script>
