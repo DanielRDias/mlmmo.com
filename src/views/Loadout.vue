fae1a8d27b25f3788dbc373cf52852d275a38a15
Daniel Dias <danielricardodias@gmail.com>
2021-03-22 23:17:14 +0000

    feat(loadout): added loadout component

---
+++ src/views/Loadout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <Loadout :currentLoadoutId="currentLoadoutId" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Loadout from "@/components/Loadout.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Loadout,
+  },
+  data() {
+    return {
+      currentLoadoutId: this.$route.query.loadoutId,
+    };
+  },
+};
+</script>
