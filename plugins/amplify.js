03cbab2d04855c4fa67b9772d08faf5797cd1653
Daniel Dias <DanielRicardoDias@gmail.com>
2021-06-28 09:17:06 +0200

    :wrench: (amplify) update aws-export dir

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Amplify, * as AmplifyModules from '@aws-amplify/core'
 import { AmplifyPlugin } from 'aws-amplify-vue'
-import awsconfig from '~/aws-exports'
+import awsconfig from '~/src/aws-exports'
 
 Amplify.configure(awsconfig)
 
